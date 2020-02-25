18bb083dd363a0e5d7a94d8ea18296ade4946eec
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:22:09 -0500

    first commit

---
+++ variables.py
@@ -0,0 +1,23 @@
+
+name= "sidiki"
+age= 17
+
+print("my name is", name,"and I am" , age)
+addedAge= 1
+futureAge= addedAge+age
+print("Next year, I will be ", futureAge,"years old")
+
+
+randomInt=5
+randomFloat=7.2
+randomBool=True
+randomNone=None
+randomStrings="sup"
+
+print(type(randomInt))
+print(type(randomFloat))
+print(type(randomBool))
+print(type(randomNone))
+print(type(randomStrings))
+
+print("my name is "+name+"and I am "+ str(age))
